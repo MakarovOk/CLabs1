d6f0a7d0e895a48047a21e5ee41c77a9fde20b07
MakarovOk <64018096+MakarovOk@users.noreply.github.com>
2020-05-12 12:32:19 +0500

    Update lab1.6.c

--- lab1.6.c
+++ lab1.6.c
@@ -2,10 +2,14 @@
 #include <math.h>
 #include <conio.h>
 
-long int factorial(long int n)
-{
-    if (n == 0 || n == 1) return 1;
-    return n * factorial(n - 1);
+
+long int factorial(int n) {
+    int result = 1;
+
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 int main() 
